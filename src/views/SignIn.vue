d32b83955a66f8ce6d53cd43b95a2587175d38a7
yun <n856839@gmail.com>
2021-04-12 18:17:57 +0800

    Add SignIn & SignUp page

---
+++ src/views/SignIn.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="container py-5">
+    <form class="w-100" @submit.prevent.stop="handleSubmit">
+      <div class="text-center mb-4">
+        <h1 class="title h3 mb-3 font-weight-normal">
+          Sign In
+        </h1>
+      </div>
+
+      <div class="form-label-group mb-2">
+        <label for="email">email</label>
+        <input
+          id="email"
+          v-model="email"
+          name="email"
+          type="email"
+          class="form-control"
+          placeholder="email"
+          autocomplete="username"
+          required
+          autofocus
+        >
+      </div>
+
+      <div class="form-label-group mb-3">
+        <label for="password">Password</label>
+        <input
+          id="password"
+          v-model="password"
+          name="password"
+          type="password"
+          class="form-control"
+          placeholder="Password"
+          autocomplete="current-password"
+          required
+        >
+      </div>
+
+      <button
+        class="btn btn-lg btn-info btn-block mb-3"
+        type="submit"
+      >
+        Submit
+      </button>
+
+      <div class="text-center mb-3">
+        <p class="signup">
+          <router-link to="/signup">Sign Up</router-link>
+        </p>
+      </div>
+
+      <p class="mt-5 mb-3 text-muted text-center">
+        &copy; 2021
+      </p>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    handleSubmit () {      
+      const data = JSON.stringify({
+        email: this.email,
+        password: this.password
+      })
+      console.log('data', data)
+    }
+  }
+}
+</script>
+
+<style>
+  .container {
+    width: 600px;
+    font-weight: 700;
+  }
+  .title {
+    color: red;
+    background: snow;
+  }
+  .signup a {
+    text-decoration: none;
+    color: red;
+  }
+  .signup:first-child:hover  {
+    border: 1px solid skyblue
+  }
+</style>
---
+++ src/views/SignUp.vue
@@ -0,0 +1,132 @@
+<template>
+  <div class="container py-5">
+    <form
+      class="w-100"
+      @submit.prevent.stop="handleSubmit"
+    >
+      <div class="text-center mb-4">
+        <h1 class="title h3 mb-3 font-weight-normal">
+          Sign Up
+        </h1>
+      </div>
+
+      <div class="form-label-group mb-2">
+        <label for="name">Name</label>
+        <input
+          id="name"
+          v-model="name"
+          name="name"
+          type="text"
+          class="form-control"
+          placeholder="name"
+          autocomplete="username"
+          required
+          autofocus
+        >
+      </div>
+
+      <div class="form-label-group mb-2">
+        <label for="email">Email</label>
+        <input
+          id="email"
+          v-model="email"
+          name="email"
+          type="email"
+          class="form-control"
+          placeholder="email"
+          autocomplete="email"
+          required
+        >
+      </div>
+
+      <div class="form-label-group mb-3">
+        <label for="password">Password</label>
+        <input
+          id="password"
+          v-model="password"
+          name="password"
+          type="password"
+          class="form-control"
+          placeholder="Password"
+          autocomplete="new-password"
+          required
+        >
+      </div>
+
+      <div class="form-label-group mb-3">
+        <label for="password-check">Password Check</label>
+        <input
+          id="password-check"
+          v-model="passwordCheck"
+          name="passwordCheck"
+          type="password"
+          class="form-control"
+          placeholder="Password"
+          autocomplete="new-password"
+          required
+        >
+      </div>
+
+      <button
+        class="btn btn-lg btn-info btn-block mb-3"
+        type="submit"
+      >
+        Submit
+      </button>
+
+      <div class="text-center mb-3">
+        <p class="signin">
+          <router-link to="/signin">
+            Sign In
+          </router-link>
+        </p>
+      </div>
+
+      <p class="mt-5 mb-3 text-muted text-center">
+        © 2021
+      </p>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      name: '',
+      email: '',
+      password: '',
+      passwordCheck: ''
+    }
+  },
+  methods: {
+    handleSubmit () {
+      const data = JSON.stringify({
+        name: this.name,
+        email: this.email,
+        password: this.password,
+        passwordCheck: this.passwordCheck
+      })      
+      console.log('data', data)
+    }
+  }
+}
+</script>
+
+<style>
+  .container {
+    width: 600px;
+    font-weight: 700;
+  }
+  .title {
+    color: red;
+    background: snow;
+  }
+  .signin a {
+    text-decoration: none;
+    color: red;
+  }
+  .signin:first-child:hover  {
+    border: 1px solid skyblue
+  }
+</style>
