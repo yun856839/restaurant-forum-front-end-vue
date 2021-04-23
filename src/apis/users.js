800f81fdfe6ed01b4935306e6b3b3f09bc7ebd41
yun <n856839@gmail.com>
2021-04-23 19:22:04 +0800

    Add API - getTopUsers in users, Modify: integrate API add/deleteFollowing in UsersTop

--- src/apis/users.js
+++ src/apis/users.js
@@ -22,4 +22,20 @@ export default {
       headers: { Authorization: `Bearer ${getToken()}`}
     })
   },
+  addFollowing({ userId }) {
+    return apiHelper.post(`/following/${userId}`, null, {
+      headers: { Authorization: `Bearer ${getToken()}`}
+    })
+  },
+  deleteFollowing({ userId }) {
+    return apiHelper.delete(`/following/${userId}`, {
+      headers: { Authorization: `Bearer ${getToken()}`}
+    })
+  },
+  getTopUsers() {
+    return apiHelper.get('/users/top', {
+      headers: { Authorization: `Bearer ${getToken()}`}
+    })
+  },
+  
 }
--- src/views/UsersTop.vue
+++ src/views/UsersTop.vue
@@ -38,151 +38,8 @@
 <script>
 import NavTabs from "./../components/NavTabs";
 import { emptyImageFilter } from "./../utils/mixins";
-
-const dummyData = {
-  users: [
-    {
-      id: 1,
-      name: "root",
-      email: "root@example.com",
-      password: "$2a$10$16ClPTh28gde/3TnBX6N8eP4Em0trhb7GBhcMwohulJsU9ponGPhK",
-      isAdmin: true,
-      image: "https://i.imgur.com/5XlJtEs.jpg",
-      createdAt: "2021-02-15T13:04:33.000Z",
-      updatedAt: "2021-02-15T13:08:01.000Z",
-      Followers: [
-        {
-          id: 11,
-          name: "user1",
-          email: "user1@example.com",
-          password:
-            "$2a$10$9cqt0UixqUIPCraM6jXvKezOMG5ANH7WGnmis0ddnSu7bbDCdM7iW",
-          isAdmin: false,
-          image: "https://loremflickr.com/320/240?lock=2.7445859530035754",
-          createdAt: "2021-02-15T13:04:33.000Z",
-          updatedAt: "2021-02-15T13:04:33.000Z",
-          Followship: {
-            followerId: 11,
-            followingId: 1,
-            createdAt: "2021-02-16T10:10:34.000Z",
-            updatedAt: "2021-02-16T10:10:34.000Z",
-          },
-        },
-        {
-          id: 21,
-          name: "user2",
-          email: "user2@example.com",
-          password:
-            "$2a$10$4mK12LWzL/Tux72QN2NQVOk2YfctU6Ooe1uBAm/TyeGSbGEsoTalS",
-          isAdmin: false,
-          image: "https://loremflickr.com/320/240?lock=74.59861446049072",
-          createdAt: "2021-02-15T13:04:34.000Z",
-          updatedAt: "2021-02-15T13:04:34.000Z",
-          Followship: {
-            followerId: 21,
-            followingId: 1,
-            createdAt: "2021-02-17T03:01:13.000Z",
-            updatedAt: "2021-02-17T03:01:13.000Z",
-          },
-        },
-      ],
-      FollowerCount: 2,
-      isFollowed: false,
-    },
-    {
-      id: 21,
-      name: "user2",
-      email: "user2@example.com",
-      password: "$2a$10$4mK12LWzL/Tux72QN2NQVOk2YfctU6Ooe1uBAm/TyeGSbGEsoTalS",
-      isAdmin: false,
-      image: "https://loremflickr.com/320/240?lock=74.59861446049072",
-      createdAt: "2021-02-15T13:04:34.000Z",
-      updatedAt: "2021-02-15T13:04:34.000Z",
-      Followers: [
-        {
-          id: 1,
-          name: "root",
-          email: "root@example.com",
-          password:
-            "$2a$10$16ClPTh28gde/3TnBX6N8eP4Em0trhb7GBhcMwohulJsU9ponGPhK",
-          isAdmin: true,
-          image: "https://i.imgur.com/5XlJtEs.jpg",
-          createdAt: "2021-02-15T13:04:33.000Z",
-          updatedAt: "2021-02-15T13:08:01.000Z",
-          Followship: {
-            followerId: 1,
-            followingId: 21,
-            createdAt: "2021-02-16T10:10:01.000Z",
-            updatedAt: "2021-02-16T10:10:01.000Z",
-          },
-        },
-        {
-          id: 11,
-          name: "user1",
-          email: "user1@example.com",
-          password:
-            "$2a$10$9cqt0UixqUIPCraM6jXvKezOMG5ANH7WGnmis0ddnSu7bbDCdM7iW",
-          isAdmin: false,
-          image: "https://loremflickr.com/320/240?lock=2.7445859530035754",
-          createdAt: "2021-02-15T13:04:33.000Z",
-          updatedAt: "2021-02-15T13:04:33.000Z",
-          Followship: {
-            followerId: 11,
-            followingId: 21,
-            createdAt: "2021-02-16T10:10:40.000Z",
-            updatedAt: "2021-02-16T10:10:40.000Z",
-          },
-        },
-      ],
-      FollowerCount: 2,
-      isFollowed: true,
-    },
-    {
-      id: 11,
-      name: "user1",
-      email: "user1@example.com",
-      password: "$2a$10$9cqt0UixqUIPCraM6jXvKezOMG5ANH7WGnmis0ddnSu7bbDCdM7iW",
-      isAdmin: false,
-      image: "https://loremflickr.com/320/240?lock=2.7445859530035754",
-      createdAt: "2021-02-15T13:04:33.000Z",
-      updatedAt: "2021-02-15T13:04:33.000Z",
-      Followers: [
-        {
-          id: 1,
-          name: "root",
-          email: "root@example.com",
-          password:
-            "$2a$10$16ClPTh28gde/3TnBX6N8eP4Em0trhb7GBhcMwohulJsU9ponGPhK",
-          isAdmin: true,
-          image: "https://i.imgur.com/5XlJtEs.jpg",
-          createdAt: "2021-02-15T13:04:33.000Z",
-          updatedAt: "2021-02-15T13:08:01.000Z",
-          Followship: {
-            followerId: 1,
-            followingId: 11,
-            createdAt: "2021-02-20T10:17:22.000Z",
-            updatedAt: "2021-02-20T10:17:22.000Z",
-          },
-        },
-      ],
-      FollowerCount: 1,
-      isFollowed: true,
-    },
-    {
-      id: 31,
-      name: "aaaTEST",
-      email: "aaat@aaa.com",
-      password: "$2a$10$9soLNtv.7NGEuc2hKnavkOuWrZY..US1mbRzXCkJFobHGBXr5g4BC",
-      isAdmin: false,
-      image: null,
-      createdAt: "2021-02-18T07:48:10.000Z",
-      updatedAt: "2021-02-18T07:48:10.000Z",
-      Followers: [],
-      FollowerCount: 0,
-      isFollowed: false,
-    },
-  ],
-};
+import usersAPI from "./../apis/users";
+import { Toast } from "./../utils/helpers";
 
 export default {
   name: "Users",
@@ -196,40 +53,74 @@ export default {
     };
   },
   methods: {
-    fetchTopUsers() {
-      this.users = dummyData.users.map((user) => ({
-        id: user.id,
-        name: user.name,
-        image: user.image,
-        followerCount: user.FollowerCount,
-        isFollowed: user.isFollowed,
-      }));
+    async fetchTopUsers() {
+      try {
+        const { data } = await usersAPI.getTopUsers();
+        this.users = data.users.map((user) => ({
+          id: user.id,
+          name: user.name,
+          image: user.image,
+          followerCount: user.FollowerCount,
+          isFollowed: user.isFollowed,
+        }));
+      } catch (err) {
+        Toast.fire({
+          icon: "error",
+          title: "Can not get UsersTop data. Try later.",
+        });
+      }
     },
-    addFollowing(userId) {
-      this.users = this.users.map((user) => {
-        if (user.id !== userId) {
-          return user;
-        } else {
-          return {
-            ...user,
-            followerCount: user.followerCount + 1,
-            isFollowed: true,
-          };
+    async addFollowing(userId) {
+      try {
+        const { data } = await usersAPI.addFollowing({ userId });
+
+        if (data.status !== "success") {
+          throw new Error(data.message);
         }
-      });
+
+        this.users = this.users.map((user) => {
+          if (user.id !== userId) {
+            return user;
+          } else {
+            return {
+              ...user,
+              followerCount: user.followerCount + 1,
+              isFollowed: true,
+            };
+          }
+        });
+      } catch (err) {
+        Toast.fire({
+          icon: "error",
+          title: "Can not add following. Try later.",
+        });
+      }
     },
-    deleteFollowing(userId) {
-      this.users = this.users.map((user) => {
-        if (user.id !== userId) {
-          return user;
-        } else {
-          return {
-            ...user,
-            followerCount: user.followerCount - 1,
-            isFollowed: false,
-          };
+    async deleteFollowing(userId) {
+      try {
+        const { data } = await usersAPI.deleteFollowing({ userId });
+
+        if (data.status !== "success") {
+          throw new Error(data.message);
         }
-      });
+
+        this.users = this.users.map((user) => {
+          if (user.id !== userId) {
+            return user;
+          } else {
+            return {
+              ...user,
+              followerCount: user.followerCount - 1,
+              isFollowed: false,
+            };
+          }
+        });
+      } catch (err) {
+        Toast.fire({
+          icon: "error",
+          title: "Can not delete following. Try later.",
+        });
+      }
     },
   },
   created() {
