858fd6c2ede353a8f55503749e8c64f1ee45dd5d
yun <n856839@gmail.com>
2021-04-27 15:53:46 +0800

    Add API - getRestaurant in restaurants, Modify integrate API of getRestaurant and currentUser in Restaurant/RestuaurantDetail

--- src/components/RestaurantDetail.vue
+++ src/components/RestaurantDetail.vue
@@ -89,6 +89,14 @@ export default {
       restaurant: this.initialRestaurant,
     };
   },
+  watch: {
+    initialRestaurant(newValue) {
+      this.restaurant = {
+        ...this.restaurant,
+        ...newValue,
+      };
+    },
+  },
   methods: {
     addFavorite() {
       this.restaurant = {
--- src/views/Restaurant.vue
+++ src/views/Restaurant.vue
@@ -22,152 +22,9 @@
 import RestaurantDetail from "./../components/RestaurantDetail";
 import RestaurantComments from "./../components/RestaurantComments";
 import CreateComment from "./../components/CreateComment";
-
-const dummyUser = {
-  currentUser: {
-    id: 1,
-    name: "root",
-    email: "root@example.com",
-    isAdmin: true,
-  },
-  isAuthenticated: true,
-};
-
-const dummyData = {
-  restaurant: {
-    id: 1,
-    name: "Jeanette Towne",
-    tel: "0120456789",
-    address: "178 Andreanne Gateway",
-    opening_hours: "08:00",
-    description: "tenetur ut repellat",
-    image: "https://i.imgur.com/MIdlANq.jpeg",
-    CategoryId: 61,
-    viewCounts: 19,
-    createdAt: "2021-02-15T13:04:35.000Z",
-    updatedAt: "2021-04-14T07:50:48.340Z",
-    Category: {
-      id: 61,
-      name: "複合式料理",
-      createdAt: "2021-02-15T13:04:34.000Z",
-      updatedAt: "2021-02-15T13:04:34.000Z",
-    },
-    LikedUsers: [
-      {
-        id: 1,
-        name: "root",
-        email: "root@example.com",
-        password:
-          "$2a$10$16ClPTh28gde/3TnBX6N8eP4Em0trhb7GBhcMwohulJsU9ponGPhK",
-        isAdmin: true,
-        image: "https://i.imgur.com/5XlJtEs.jpg",
-        createdAt: "2021-02-15T13:04:33.000Z",
-        updatedAt: "2021-02-15T13:08:01.000Z",
-        Like: {
-          UserId: 1,
-          RestaurantId: 1,
-          createdAt: "2021-02-19T04:38:08.000Z",
-          updatedAt: "2021-02-19T04:38:08.000Z",
-        },
-      },
-    ],
-    FavoritedUsers: [
-      {
-        id: 1,
-        name: "root",
-        email: "root@example.com",
-        password:
-          "$2a$10$16ClPTh28gde/3TnBX6N8eP4Em0trhb7GBhcMwohulJsU9ponGPhK",
-        isAdmin: true,
-        image: "https://i.imgur.com/5XlJtEs.jpg",
-        createdAt: "2021-02-15T13:04:33.000Z",
-        updatedAt: "2021-02-15T13:08:01.000Z",
-        Favorite: {
-          UserId: 1,
-          RestaurantId: 1,
-          createdAt: "2021-02-16T05:31:43.000Z",
-          updatedAt: "2021-02-16T05:31:43.000Z",
-        },
-      },
-      {
-        id: 11,
-        name: "user1",
-        email: "user1@example.com",
-        password:
-          "$2a$10$9cqt0UixqUIPCraM6jXvKezOMG5ANH7WGnmis0ddnSu7bbDCdM7iW",
-        isAdmin: false,
-        image: "https://loremflickr.com/320/240?lock=2.7445859530035754",
-        createdAt: "2021-02-15T13:04:33.000Z",
-        updatedAt: "2021-02-15T13:04:33.000Z",
-        Favorite: {
-          UserId: 11,
-          RestaurantId: 1,
-          createdAt: "2021-02-16T10:10:44.000Z",
-          updatedAt: "2021-02-16T10:10:44.000Z",
-        },
-      },
-      {
-        id: 21,
-        name: "user2",
-        email: "user2@example.com",
-        password:
-          "$2a$10$4mK12LWzL/Tux72QN2NQVOk2YfctU6Ooe1uBAm/TyeGSbGEsoTalS",
-        isAdmin: false,
-        image: "https://loremflickr.com/320/240?lock=74.59861446049072",
-        createdAt: "2021-02-15T13:04:34.000Z",
-        updatedAt: "2021-02-15T13:04:34.000Z",
-        Favorite: {
-          UserId: 21,
-          RestaurantId: 1,
-          createdAt: "2021-02-17T03:00:30.000Z",
-          updatedAt: "2021-02-17T03:00:30.000Z",
-        },
-      },
-    ],
-    Comments: [
-      {
-        id: 31,
-        text: "recusandae tempora molestiae",
-        UserId: 21,
-        RestaurantId: 1,
-        createdAt: "2021-02-15T13:04:35.000Z",
-        updatedAt: "2021-02-15T13:04:35.000Z",
-        User: {
-          id: 21,
-          name: "user2",
-          email: "user2@example.com",
-          password:
-            "$2a$10$4mK12LWzL/Tux72QN2NQVOk2YfctU6Ooe1uBAm/TyeGSbGEsoTalS",
-          isAdmin: false,
-          image: "https://loremflickr.com/320/240?lock=74.59861446049072",
-          createdAt: "2021-02-15T13:04:34.000Z",
-          updatedAt: "2021-02-15T13:04:34.000Z",
-        },
-      },
-      {
-        id: 81,
-        text: "hi",
-        UserId: 1,
-        RestaurantId: 1,
-        createdAt: "2021-02-15T13:06:18.000Z",
-        updatedAt: "2021-02-15T13:06:18.000Z",
-        User: {
-          id: 1,
-          name: "root",
-          email: "root@example.com",
-          password:
-            "$2a$10$16ClPTh28gde/3TnBX6N8eP4Em0trhb7GBhcMwohulJsU9ponGPhK",
-          isAdmin: true,
-          image: "https://i.imgur.com/5XlJtEs.jpg",
-          createdAt: "2021-02-15T13:04:33.000Z",
-          updatedAt: "2021-02-15T13:08:01.000Z",
-        },
-      },
-    ],
-  },
-  isFavorited: true,
-  isLiked: true,
-};
+import restaurantsAPI from "./../apis/restaurants";
+import { Toast } from "./../utils/helpers";
+import { mapState } from "vuex";
 
 export default {
   name: "Restaurant",
@@ -191,40 +48,58 @@ export default {
         isLiked: false,
       },
       restaurantComments: [],
-      currentUser: dummyUser.currentUser,
     };
   },
+  computed: {
+    ...mapState(["currentUser"]),
+  },
+  beforeRouteUpdate(to, from, next) {
+    const { id } = to.params;
+    this.fetchRestaurant(id);
+    next();
+  },
   methods: {
-    fetchRestaurant(restaurantId) {
-      console.log("fetchRestaurant id: ", restaurantId);
+    async fetchRestaurant(restaurantId) {
+      try {
+        const { data } = await restaurantsAPI.getRestaurant({ restaurantId });
 
-      const { restaurant, isFavorited, isLiked } = dummyData;
-      const {
-        id,
-        name,
-        Category,
-        image,
-        opening_hours,
-        tel,
-        address,
-        description,
-        Comments,
-      } = restaurant;
+        if (data.status === "error") {
+          throw new Error(data.message);
+        }
 
-      this.restaurant = {
-        id,
-        name,
-        categoryName: Category ? Category.name : "未分類",
-        image,
-        openingHours: opening_hours,
-        tel,
-        address,
-        description,
-        isFavorited,
-        isLiked,
-      };
+        const { restaurant, isFavorited, isLiked } = data;
+        const {
+          id,
+          name,
+          Category,
+          image,
+          opening_hours,
+          tel,
+          address,
+          description,
+          Comments,
+        } = restaurant;
 
-      this.restaurantComments = Comments;
+        this.restaurant = {
+          id,
+          name,
+          categoryName: Category ? Category.name : "未分類",
+          image,
+          openingHours: opening_hours,
+          tel,
+          address,
+          description,
+          isFavorited,
+          isLiked,
+        };
+
+        this.restaurantComments = Comments;
+      } catch (err) {
+        Toast.fire({
+          icon: "error",
+          title: "Can not get restaurant data. Try later.",
+        });
+      }
     },
     afterDeleteComment(commentId) {
       console.log("afterDeleteComment", commentId);
